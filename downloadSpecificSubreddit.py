3f6648bd5b509037c49116ee8a69472e68b66b13
Harshad <harshad@coseer.com>
2019-07-31 16:09:56 +0530

    initial commit

---
+++ downloadSpecificSubreddit.py
@@ -0,0 +1,36 @@
+import requests 
+import json
+import pymongo
+from datetime import datetime
+import time
+#now = time.time()
+
+connection = pymongo.MongoClient("mongodb://localhost")  
+db=connection.joke
+record1 = db.joke_collection
+
+# api-endpoint 
+#print(type(now))
+#print(now)
+#last = int(now - 100);
+last = '1549537111'
+i = 0
+URL = "https://api.pushshift.io/reddit/search/submission/"
+while i < 1000 :  
+	print(last)
+	# defining a params dict for the parameters to be sent to the API 
+	PARAMS = {'subreddit':'jokes', 'sort':'desc', 'sort_type':'created_utc', 'size':'1000', 'before':last} 
+	  
+	# sending get request and saving the response as response object 
+	r = requests.get(url = URL, params = PARAMS) 
+	  
+	# extracting data in json format 
+	your_data = r.json()
+
+	jokes = your_data['data']
+	for joke in jokes:
+		print("Adding...")
+		last = joke["created_utc"]
+		record1.insert(joke)
+	i = i + 1
+	time.sleep(10)
---
+++ retrieveMongo.py
@@ -0,0 +1,32 @@
+import pymongo
+import nltk
+from nltk.tokenize import word_tokenize
+from sklearn.feature_extraction.text import CountVectorizer
+from nltk.corpus import stopwords
+
+connection = pymongo.MongoClient("mongodb://localhost")  
+db=connection.joke
+record1 = db.joke_collection
+nsfw_words = [];
+sents = []
+for x in record1.find({},{ "title": 1, "selftext": 2, "over_18":3 }):
+  #print(x)
+  if not 'selftext' in x:
+  	continue
+  if x['over_18'] == True:
+  		sent = x['title'] + ' '+x['selftext']
+  		stop_words = set(stopwords.words('english')) 
+  		word_tokens = word_tokenize(sent) 
+  		filtered = [w for w in word_tokens if not w in stop_words]
+  		filtered_sentence = ''
+  		for w in filtered:
+  			filtered_sentence = w + ' '
+  		sents.append(filtered_sentence.strip())
+  		#print(sent)
+print(sents)
+vec = CountVectorizer().fit(sents)
+bag_of_words = vec.transform(sents)
+sum_words = bag_of_words.sum(axis=0) 
+words_freq = [(word, sum_words[0, idx]) for word, idx in     vec.vocabulary_.items()]
+words_freq =sorted(words_freq, key = lambda x: x[1], reverse=True)
+print(words_freq[:20])
